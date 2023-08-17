20edebb30a992843dc5fce82ceff9fa608cad1fe
PrimusAbOris <guy.ross.rit@gmail.com>
2023-08-17 08:49:58 -0400

    Submittable version created

--- src/com/coderscampus/GR_Assignment2.java
+++ src/com/coderscampus/GR_Assignment2.java
@@ -2,67 +2,42 @@ package com.coderscampus;
 
 import java.util.Random;
 import java.util.Scanner;
-import java.lang.Thread;
 
 public class GR_Assignment2 {
-
+// PROFESSIONAL VERSION. Outputs match the assignment prompt exactly.
 	public static void main(String[] args) throws InterruptedException {
 		Random zohar = new Random();
 		int monad = zohar.nextInt(100) + 1;
-		// knightOfFire(132);
-		// testing......
-		System.out.println("Begin!");
 		
 		for (int i = 5; i >= 1; i--) {
-			System.out.println("GUESSES LEFT: " + i);
-			System.out.println("Type in a number between 1 and 100.");
+			System.out.print("Pick a number between 1 and 100 ");
 			Scanner scanner = new Scanner(System.in);
 			String input = scanner.nextLine();
 			Integer dyad = Integer.parseInt(input);
-			
-			// win condition; comment this out
-			// dyad = monad;
+
 			
 			if ((dyad < 1) | (dyad > 100)) {
-				System.out.println("I am not familiar with such a number."
-					+ "\nThere are only the counting numbers from 1 to 100.\n"
-					+ "Your guess has been refunded.");
-				i++;
+				System.out.println("Your guess is not between 1 and 100, please try again");
 			}
 			else {
 				if (monad == dyad) {
-					System.out.println("BINGO!\n"
-							+ "This message will self-destruct in 4 seconds!");
-					Thread.sleep(4000);
-					// test message before returning
-					System.out.println("Returning after a victory");
+					System.out.println("You win!");
 					return;
 				}
 				else {
-					System.out.print("Incorrect! Your number was ");
+					System.out.print("Please pick a ");
 					if (monad > dyad) {
-						System.out.println("TOO LOW!");
+						System.out.println("higher number");
 					} else if (monad < dyad) {
-						System.out.println("TOO HIGH!");
+						System.out.println("lower number");
 					}
 				}
 			}
 			
 		}
 		System.out.println("It's a sad thing that your adventures have ended here!\n"
-				+ "The number was " + monad + ".");
-		// test message before returning
-		System.out.println("Returning after a loss");
+				+ "The number to guess was " + monad + ".");
 		return;
-	}
-	
-	public static void knightOfFire(int bpm) throws InterruptedException {
-		int s = (60000 / bpm) * 4;
-		String sen = "TOTAL SENTENCE IMPOSED IS TEN";
-		for (int k = 4; k >= 1; k--) {
-			System.out.println(sen);
-			Thread.sleep(s);
-		}
 	}
 
 }
---
+++ src/com/coderscampus/REAL_GR_Assignment2.java
@@ -0,0 +1,67 @@
+package com.coderscampus;
+
+import java.util.Random;
+import java.util.Scanner;
+import java.lang.Thread;
+
+public class REAL_GR_Assignment2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		Random zohar = new Random();
+		int monad = zohar.nextInt(100) + 1;
+		// knightOfFire(132);
+		System.out.println("Begin!");
+		
+		for (int i = 5; i >= 1; i--) {
+			System.out.println("GUESSES LEFT: " + i);
+			System.out.println("Type in a number between 1 and 100.");
+			Scanner scanner = new Scanner(System.in);
+			String input = scanner.nextLine();
+			Integer dyad = Integer.parseInt(input);
+			
+			// win condition; comment this out
+			// dyad = monad;
+			
+			if ((dyad < 1) | (dyad > 100)) {
+				System.out.println("I am not familiar with such a number."
+					+ "\nThere are only the counting numbers from 1 to 100.\n"
+					+ "Your guess has been refunded.");
+				i++;
+			}
+			else {
+				if (monad == dyad) {
+					System.out.println("BINGO!\n"
+							+ "This message will self-destruct in 4 seconds!");
+					Thread.sleep(4000);
+					// test message before returning
+					System.out.println("Returning after a victory");
+					return;
+				}
+				else {
+					System.out.print("Incorrect! Your number was ");
+					if (monad > dyad) {
+						System.out.println("TOO LOW!");
+					} else if (monad < dyad) {
+						System.out.println("TOO HIGH!");
+					}
+				}
+			}
+			
+		}
+		System.out.println("It's a sad thing that your adventures have ended here!\n"
+				+ "The number was " + monad + ".");
+		// test message before returning
+		System.out.println("Returning after a loss");
+		return;
+	}
+	
+	public static void knightOfFire(int bpm) throws InterruptedException {
+		int s = (60000 / bpm) * 4;
+		String sen = "TOTAL SENTENCE IMPOSED IS TEN";
+		for (int k = 4; k >= 1; k--) {
+			System.out.println(sen);
+			Thread.sleep(s);
+		}
+	}
+
+}
